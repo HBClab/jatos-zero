2edecab7bdc4f58036b352ec519bc21238665763
Zak Gilliam <zakgilliam@gmail.com>
2026-03-12 09:13:48 -0500

    feat: logging of errors and fix on days_ago

--- beh/main_handler.py
+++ beh/main_handler.py
@@ -313,7 +313,7 @@ class Handler:
             proxy=False
         )
 
-        txt_dfs = pull_instance.load(days_ago=127)
+        txt_dfs = pull_instance.load(days_ago=365)
         return self.convert_to_csv(txt_dfs, task)
 
     def convert_to_csv(self, txt_dfs, task):
